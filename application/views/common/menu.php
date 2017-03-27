0aa902a76aaef68482a03713da1be750c05e9312
pujie <pw.prayitno@gmail.com>
2017-03-27 09:11:42 +0700

    separate menu and navigation on views

---
+++ application/views/common/menu.php
@@ -0,0 +1,41 @@
+<ul class="nav nav-list">
+    <li class="">
+        <a href="index.html">
+            <i class="menu-icon fa fa-tachometer"></i>
+            <span class="menu-text"> Dashboard </span>
+        </a>
+
+        <b class="arrow"></b>
+    </li>
+    <li class="active open">
+        <a href="#" class="dropdown-toggle">
+            <i class="menu-icon fa fa-list"></i>
+            <span class="menu-text"> Master </span>
+
+            <b class="arrow fa fa-angle-down"></b>
+        </a>
+
+        <b class="arrow"></b>
+
+        <ul class="submenu">
+            <li class="active">
+                <a href="tables.html">
+                    <i class="menu-icon fa fa-caret-right"></i>
+                    User
+                </a>
+
+                <b class="arrow"></b>
+            </li>
+
+            <li class="">
+                <a href="jqgrid.html">
+                    <i class="menu-icon fa fa-caret-right"></i>
+                    Pelanggan
+                </a>
+
+                <b class="arrow"></b>
+            </li>
+        </ul>
+    </li>
+
+</ul><!-- /.nav-list -->
